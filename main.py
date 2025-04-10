e63539e86c826dca6564d287275436ae3eb13adf
sierra <sierrajhonatan74@gmail.com>
2025-04-10 16:48:10 -0500

    Modulos

---
+++ main.py
@@ -0,0 +1,9 @@
+import matematicas
+
+a = 10
+b = 5
+
+print("Suma:", matematicas.suma(a, b))
+print("Resta:", matematicas.resta(a, b))
+print("Multiplicación:", matematicas.multiplicacion(a, b))
+print("División:", matematicas.division(a, b))
